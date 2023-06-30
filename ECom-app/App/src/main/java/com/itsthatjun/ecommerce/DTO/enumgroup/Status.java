580fffdc004b29f5a86c415f02e773ac3833a632
JunChen22 <ccjh522@gmail.com>
2023-06-29 22:16:52 -0400

    added SMS service

---
+++ ECom-app/App/src/main/java/com/itsthatjun/ecommerce/DTO/enumgroup/Status.java
@@ -0,0 +1,6 @@
+package com.itsthatjun.ecommerce.DTO.enumgroup;
+
+public enum Status {
+    ACTIVE,
+    INACTIVE;
+}
--- ECom-app/SMS/src/main/java/com/itsthatjun/ecommerce/service/impl/SalesServiceimpl.java
+++ ECom-app/SMS/src/main/java/com/itsthatjun/ecommerce/service/impl/SalesServiceimpl.java
@@ -1,11 +1,61 @@
 package com.itsthatjun.ecommerce.service.impl;
 
+import com.itsthatjun.ecommerce.mbg.mapper.*;
+import com.itsthatjun.ecommerce.mbg.model.*;
+import com.itsthatjun.ecommerce.service.SalesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class SalesServiceimpl {
+public class SalesServiceimpl implements SalesService {
+
+    private final PromotionSaleMapper promotionSaleMapper;
+
+    private final PromotionSaleProductRelationMapper productRelationMapper;
+
+    private final ProductSkuStockMapper productSkuStockMapper;
+
+    private final PromotionSaleLogMapper promotionSaleLogMapper;
+
+    private final ProductMapper productMapper;
+
+    @Autowired
+    public SalesServiceimpl(PromotionSaleMapper promotionSaleMapper, PromotionSaleProductRelationMapper productRelationMapper,
+                            ProductSkuStockMapper productSkuStockMapper, PromotionSaleLogMapper promotionSaleLogMapper,
+                            ProductMapper productMapper) {
+        this.promotionSaleMapper = promotionSaleMapper;
+        this.productRelationMapper = productRelationMapper;
+        this.productSkuStockMapper = productSkuStockMapper;
+        this.promotionSaleLogMapper = promotionSaleLogMapper;
+        this.productMapper = productMapper;
+    }
+
+    @Override
+    public List<PromotionSale> getAllPromotionalSale() {
+        PromotionSaleExample saleExample = new PromotionSaleExample();
+        saleExample.createCriteria().andStatusEqualTo(1);  // default active sales
+        List<PromotionSale> result = promotionSaleMapper.selectByExample(saleExample);
+        return result.size() > 0 ? result : null;
+    }
 
     // 0-> not on sale; 1-> is on sale; 2-> flash sale/special sales/clarance/used item
-    // TODO: add in sales service
+
+    @Override
+    public List<Product> getAllPromotionalSaleItems() {
+        ProductExample productExample = new ProductExample();
+        productExample.createCriteria().andOnSaleStatusEqualTo(1);
+        List<Product> result = productMapper.selectByExample(productExample);
+        return result.size() > 0 ? result : null;
+    }
+
+    @Override
+    public List<Product> getAllFlashSaleItems() {
+        ProductExample productExample = new ProductExample();
+        productExample.createCriteria().andOnSaleStatusEqualTo(2);
+        List<Product> result = productMapper.selectByExample(productExample);
+        return result.size() > 0 ? result : null;
+    }
 
 }
