85f54490fdae9a1b2c1c06a783c4ee57db75fe74
JunChen22 <ccjh522@gmail.com>
2023-11-16 14:00:49 -0500

    change helm charts

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -7,7 +7,7 @@
 
 kubectl create namespace e-com
 
-# initialization data.
+# initialization data.q
 kubectl create configmap postgres-data --from-file=../document/data.sql --namespace e-com
 
 helm install e-com-dev-env ./environments/dev-env --namespace e-com
